e6b9ff049ae2b3a8136419f46fc322531b002b22
Mikhail <kamenevmm@gmail.com>
2022-10-03 22:58:38 +0300

    дз готово

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+        String fullName1 = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName1);
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName3 = fullName2.replace("ё", "е");
+        System.out.println(fullName3);
     }
 }
